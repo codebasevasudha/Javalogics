ea7e65543c6b56c8f86e8446a9a980d89402c624
Vasudha <vasudhavyas02@gmail.com>
2025-10-07 11:57:25 +0400

    Changes in root folder

--- src/main/java/FirstNonRepCharInString.java
+++ src/main/java/FirstNonRepCharInString.java
@@ -4,10 +4,10 @@ import java.util.Map;
 public class FirstNonRepCharInString {
 
     /*
-    * First non repeative charachter in string
-    * String str = vasudha
-    *
-    * */
+     * First non repeative charachter in string
+     * String str = vasudha
+     *
+     * */
     public static void main(String[] args) {
         LinkedHashMap<Character, Integer> map = new LinkedHashMap();
         String str = "vasudha";
