62d6c9dbedd53d81ba463f50056e0b0220951679
Vasudha <vasudhavyas02@gmail.com>
2025-10-07 11:34:41 +0400

    First Non Repeative char from string in java

---
+++ src/src/FirstNonRepCharInString.java
@@ -0,0 +1,26 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepCharInString {
+
+    /*
+    * First non repeative charachter in string
+    * String str = vasudha
+    *
+    * */
+    public static void main(String[] args) {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap();
+        String str = "vasudha";
+        for(char ch: str.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+                System.out.println("First non repeative "+entry.getKey());
+                break;
+            }
+        }
+
+    }
+}
